be9ed88f5bb71e4682e06c2bf7f50b0a612998f8
Paulo Abelha Ferreira <pauloabelha@gmail.com>
2018-07-15 16:30:35 +0100

    abcd

--- train_net.py
+++ train_net.py
@@ -34,6 +34,8 @@ else:
     optimizer = get_adadelta(net)
     start_batch_idx = 0
 
+if use_cuda:
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
 accum_net_loss = 0.
 accum_report_loss = 0.
@@ -51,7 +53,21 @@ for batch_idx, (data, target) in enumerate(synthom_loader):
     rgbd, obj_id, obj_pose, target_joints, target_heatmaps = Variable(rgbd), Variable(obj_id),\
                                      Variable(obj_pose), Variable(target_joints), \
                                      Variable(target_heatmaps)
+    if use_cuda:
+        rgbd = rgbd.cuda()
+        obj_id = obj_id.cuda()
+        obj_pose = obj_pose.cuda()
+        target_joints = target_joints.cuda()
+
     output = net(data)
+
+    if use_cuda:
+        output_main_np = output[7].cpu().data.numpy()
+        target_joints_np = target_joints.cpu().data.numpy()
+    else:
+        output_main_np = output[7].data.numpy()
+        target_joints_np = target_joints.data.numpy()
+
     weights_heatmaps_loss, weights_joints_loss = get_loss_weights(batch_idx)
     loss_func = cross_entropy_loss_p_logq
     loss, loss_heatmaps, loss_joints, loss_main_joints = calculate_loss_JORNet(
@@ -63,7 +79,7 @@ for batch_idx, (data, target) in enumerate(synthom_loader):
     optimizer.zero_grad()
 
     accum_net_loss += loss.item()
-    accum_report_loss += calc_avg_joint_loss(output[7].data.numpy(), target_joints.data.numpy())
+    accum_report_loss += calc_avg_joint_loss(output_main_np, target_joints_np)
 
     if batch_idx > 0 and batch_idx % log_interv == 0:
         idx_a = 0
